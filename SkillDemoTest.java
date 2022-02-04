94968ef6ced4b24d1f4136301f9ce490baab9481
hoz011 <97712975+hoz011@users.noreply.github.com>
2022-02-04 14:30:40 -0800

    lib + test

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void testIsEven() {
+        assertEquals(true, SkillDemo.isEvenSum(4, 3));
+    }
+}

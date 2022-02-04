fff9a9495f140c720cc9241b23fbce7102cf517a
hoz011 <97712975+hoz011@users.noreply.github.com>
2022-02-04 14:28:54 -0800

    Update SkillDemo.java

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,9 @@
 public class SkillDemo {
-// implement later
+    public static boolean isEvenSum(int a, int b) {
+        if ((a + b) % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
